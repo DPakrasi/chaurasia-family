6b37b2732c7d352362944e6db5a91b4a4e61cf77
DPakrasi <pakrasideepsa@gmail.com>
2024-02-27 22:48:16 -0500

    Added navigation

--- src/Marketing/Header/Header.tsx
+++ src/Marketing/Header/Header.tsx
@@ -1,15 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {logo} from '../../assets';
 import {Flyout, FlyoutTrigger, FlyoutItems, FlyoutItem} from '../../shared';
 import {headerStyles} from './HeaderStyles';
+
 const {wrapperFlex} = headerStyles;
 
 const Header: React.FC = () => {
+  // Here any should be replaced by your specific navigation type or add a generic type
+  // Check - https://reactnavigation.org/docs/typescript/#specifying-default-types-for-usenavigation-link-ref-etc
+  const navigation = useNavigation<any>();
   const numberOfMenuItems = ['Login', 'Register'];
   const handleItemPress = (item: string) => {
-    console.log('item', item);
+    navigation.navigate('account-management', {
+      screen: item === 'Login' ? 'login' : 'registration',
+    });
   };
 
   return (
