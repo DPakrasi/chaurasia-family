2c37f104dfed48be84c3c0be0c075df682ebdb6a
DPakrasi <pakrasideepsa@gmail.com>
2024-05-20 14:09:21 -0400

    testing

--- src/Marketing/Marketing.tsx
+++ src/Marketing/Marketing.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import Header from './Header/Header';
@@ -12,8 +13,30 @@ const Marketing: React.FC = () => {
       contentInsetAdjustmentBehavior="automatic"
       style={backgroundStyle}>
       <Header />
-      <View>
-        <Text>Remaning Content</Text>
+      <View
+        style={{
+          paddingHorizontal: 10,
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <View
+            style={{
+              width: 48,
+              height: 48,
+              borderRadius: 24,
+              backgroundColor: 'red',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>1</Text>
+          </View>
+          <View style={{flex: 1}}>
+            <Text>Heading UHC Rewards</Text>
+            <Text>
+              Please review the terms of consitions, Privacy Policy and the GINA
+              authorization to get started.
+            </Text>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
